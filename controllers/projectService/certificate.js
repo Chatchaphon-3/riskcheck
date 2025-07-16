48d970b3794ecce720e934683dcecb478cb2a176
Chatchaphon-3 <chatchaphon2547@gmail.com>
2025-07-17 02:12:50 +0700

    eerer

--- controllers/projectService/certificate.js
+++ controllers/projectService/certificate.js
@@ -23,48 +23,13 @@ exports.createCertificate = async (req,res,next)=>{
         let certificate;
         //Make Certificate
         if(project.projectStatus === 'Completed'){ //project.projectStatus === 'Completed'
-            let unit = await Unit.findOne({projectID : req.params.id});
-            let risk = await Risk.findOne({projectID : req.params.id});
-            let eva = await User.findById(project.evaluatorID);
-            const existed = await fileExists('bucket1' , `certificate/${project.projectNum}.pdf`);
-            if(existed){ //existed
-                console.log('11111');
-                const command = new GetObjectCommand({Bucket : 'bucket1' , Key : `certificate/${project.projectNum}.pdf`});
-                const signedUrl = await getSignedUrl(s3Public , command , {expiresIn : 3600});
-                certificate = signedUrl;
-            }else{
-                console.log('999999');
-                // ==== อัปโหลดเข้า MinIO ====
-                // console.log('testing uitls/buildCertificate');
-                let BUCKET_NAME = 'bucket1';
-                try {
-                // ตรวจสอบว่า bucket มีอยู่หรือไม่
-                await s3.send(new HeadBucketCommand({ Bucket: BUCKET_NAME }));
-                // console.log("Bucket exists.");
-                } catch (err) {
-                    if (err.name === 'NotFound' || err.$metadata?.httpStatusCode === 404) {
-                        // Bucket ไม่มี → สร้างใหม่
-                        await s3.send(new CreateBucketCommand({ Bucket: BUCKET_NAME }));
-                        // console.log("Created new bucket.");
-                    } else {
-                        // ถ้าเป็น error อื่น เช่น permission, credential ผิด
-                        throw { code: 550, status: 500, message: 'ไม่สามารถเข้าถึง MinIO ได้: ' + err.message };
-                    }
-                }
-                let filename = await generateRiskCertificatePDF(project , unit , risk , eva);
-                // console.log(filename);
-                //find
                 let command = new GetObjectCommand({
                 Bucket: 'bucket1',
-                Key: filename,
+                Key: `certificate/${project.projectNum}.pdf`,
                 ResponseContentDisposition: 'attachment',
                 });
-
                 let signedUrl = await getSignedUrl(s3Public, command, { expiresIn: 3600 });
                 certificate = signedUrl;
-                // console.log('using this path' , certificate);
-                // return res.status(200).json({ success: true, certificate: signedUrl });
-            }
             console.log(certificate);
             return res.status(200).json({success : true , certificate : certificate})
         
